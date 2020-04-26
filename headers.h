16c1da6be6e8bf130ebc295e21a77d0ce53d13d7
RahulThormote <rahulthormote@yahoo.com>
2020-04-27 00:06:26 +0530

    Create headers.h

---
+++ headers.h
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<sys/ipc.h>
+#include<sys/msg.h>
+#include<arpa/inet.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include<netinet/in.h>
+
